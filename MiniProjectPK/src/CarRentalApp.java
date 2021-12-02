72786adfacdfc1372b6efaa1270ec254093c6425
PKerins97 <paulkerins123@gmail.com>
2021-12-02 15:23:49 +0000

    Mini Project

--- MiniProjectPK/src/CarRentalApp.java
+++ MiniProjectPK/src/CarRentalApp.java
@@ -1,4 +1,5 @@
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,10 +11,19 @@ public class CarRentalApp extends JFrame implements ActionListener {
     private static JButton button;
     private static JButton loginButton;
     private static JLabel passwordLabel;
-    private static JLabel userLabel;
+    private static JLabel label;
     private static JLabel success;
     private static JPasswordField passwordText;
-    private static JTextField userText;
+    private static JTextField Text;
+    private static JButton addButton;
+    private static JButton displayButton;
+    private static JButton carButton;
+    private static JButton resButton;
+    private static JButton deleteButton;
+    private static JButton makeResButton;
+    private static JButton cancelResButton;
+    Container cPane = getContentPane();
+
     //JLabel displayField;
     //ImageIcon image;
 
@@ -22,12 +32,14 @@ public class CarRentalApp extends JFrame implements ActionListener {
     private Car car;
 
 
+
     public CarRentalApp() {
 
 
         JFrame frame = new JFrame("Menu");
         setBounds(400, 100, 500, 500);
         frame.setSize(600, 400);
+        setLayout(new FlowLayout());
         frame.setLocationRelativeTo(null);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,7 +57,25 @@ public class CarRentalApp extends JFrame implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent p) {
+
+        //here you can put the code to decide what will happen should the user click on the
+        //"Car" or "Make Reservation" buttons
+
+        //System.out.println("Pressed one of the buttons!"); //just a bit of test code
+        if(p.getSource()== addButton){
+            addCar();
+        }
+        else if(p.getSource()== displayButton){
+
+            displayCar();
+        }
+        else if(p.getSource()== deleteButton){
+            deleteCar();
+        }
+
+
+
 
     }
 
@@ -56,20 +86,21 @@ public class CarRentalApp extends JFrame implements ActionListener {
             JFrame loginFrame = new JFrame("Login ");
             setBounds(400, 100, 500, 500);
             loginFrame.setSize(300, 300);
+            setLayout(new FlowLayout());
             loginFrame.setLocationRelativeTo(null);
 
 
             JPanel panel = new JPanel();
             loginFrame.add(panel);
-            userLabel = new JLabel("UserName");
-            userLabel.setBounds(10, 50, 80, 25);
-            panel.add(userLabel);
-            userText = new JTextField(20);
-            userText.setBounds(100, 50, 165, 25);
-            panel.add(userText);
+            label = new JLabel("UserName");
+            label.setBounds(10, 50, 80, 25);
+            panel.add(label);
+            Text = new JTextField(20);
+            Text.setBounds(100, 50, 165, 25);
+            panel.add(Text);
 
             passwordLabel = new JLabel("Password");
-            userLabel.setBounds(10, 80, 80, 25);
+            label.setBounds(10, 80, 80, 25);
             panel.add(passwordLabel);
 
             passwordText = new JPasswordField(20);
@@ -88,143 +119,199 @@ public class CarRentalApp extends JFrame implements ActionListener {
 
             loginFrame.setVisible(true);
         }
+    }//end of class loginAction - closing } added by JB
 
-        class LoggedIn implements ActionListener {
-
-            public void actionPerformed(ActionEvent e) {
-
-                String user = userText.getText();
-                String password = passwordText.getText();
-
-                if (user.equals("user") && password.equals("password")) {
-                    JFrame frame = new JFrame("Main Menu ");
-                    setBounds(400, 100, 500, 500);
-                    frame.setSize(700, 700);
-                    frame.setLocationRelativeTo(null);
-                    JPanel panel = new JPanel();
-                    frame.add(panel);
+    class LoggedIn implements ActionListener {
 
 
-                    JButton carButton = new JButton("Car");
-                    //carButton.addActionListener(new CarMenu());
-                    panel.add(carButton);
+        public void actionPerformed(ActionEvent e) {
 
-                    JButton addButton = new JButton("Add Car");
-                    addButton.setBounds(100, 80, 80, 25);
-                    addButton.setBackground(Color.cyan);
-                    //resButton.addActionListener(new AddCar());
-                    panel.add(addButton);
+            String user = Text.getText();
+            String password = passwordText.getText();
+
+            if (user.equals("user") && password.equals("password")) {
+                JFrame frame = new JFrame("Main Menu ");
+                setBounds(400, 100, 500, 500);
+                frame.setSize(700, 700);
+                frame.setLocationRelativeTo(null);
+                setLayout(new FlowLayout());
+                JPanel panel = new JPanel();
+                frame.add(panel);
 
 
-                    JButton resButton = new JButton("Make Reservation");
-                    resButton.setBounds(100, 80, 80, 25);
-                    resButton.setBackground(Color.cyan);
-                    //resButton.addActionListener(new Reservation());
-                    panel.add(resButton);
+                JButton carButton = new JButton("Cars");
+                carButton.setBounds(100, 80, 80, 25);
+                carButton.setBackground(Color.cyan);
+                carButton.addActionListener(new CarMenu()); //JB modified original code here
+                panel.add(carButton);
 
-                    frame.setVisible(true);
-                } else {
-                    success.setText("Invalid username or password. Try Again!!");
-                }
 
+                JButton resButton = new JButton("Reservations");
+                resButton.setBounds(100, 80, 80, 25);
+                resButton.setBackground(Color.cyan);
+                resButton.addActionListener(new ReservationMenu()); //JB modified original code here
+                panel.add(resButton);
+
+                frame.setVisible(true);
+            } else {
+                success.setText("Invalid username or password. Try Again!!");
             }
-
 
         }
 
         class CarMenu implements ActionListener {
 
+
             public void actionPerformed(ActionEvent e) {
-
-                JFrame frame = new JFrame("Add Car");
-                JButton addButton = new JButton("Add Car");
-                JPanel panel = new JPanel();
+                JFrame frame = new JFrame("Car Menu");
                 setBounds(400, 100, 500, 500);
-                frame.setSize(700, 700);
+                frame.setSize(600, 400);
                 frame.setLocationRelativeTo(null);
+                setLayout(new FlowLayout());
+                JPanel panel = new JPanel();
+                frame.add(panel);
+
+                JButton addButton = new JButton("Add Car");
+                addButton.setBounds(100, 80, 80, 25);
                 addButton.setBackground(Color.cyan);
-                //addButton.addActionListener(addCar());
                 panel.add(addButton);
+                addButton.addActionListener(this);
+
+
+                displayButton = new JButton("Display Cars");
+                displayButton.setBounds(100, 80, 80, 25);
+                displayButton.setBackground(Color.cyan);
+                panel.add(displayButton);
+                displayButton.addActionListener(this);//JB modified original code here
+
+                deleteButton = new JButton("Display Cars");
+                deleteButton.setBounds(100, 80, 80, 25);
+                deleteButton.setBackground(Color.cyan);
+                panel.add(deleteButton);
+                deleteButton.addActionListener(this);
+                frame.setVisible(true);
 
             }
-        }
-       public static void main(String[] args) {
-           new CarRentalApp();
-       }
-        public void addCar() {
-
-            final String[] carsCategory = {"Sports", "Economy", "Premium"};
-            String category;
-            String make;
-            String model;
-            String resNumber;
-            double price;
-
-            category = (String) JOptionPane.showInputDialog(null, "Add Car", "Car", JOptionPane.QUESTION_MESSAGE, null, carsCategory, carsCategory[0]);
-
-            make = JOptionPane.showInputDialog("Enter car Make");
-            //validation that a price has been entered
-            model = JOptionPane.showInputDialog("Enter car Model");
-            //validation that a price has been entered
-            resNumber = JOptionPane.showInputDialog("Enter car Registration Number");
-            //validation code to see if its only letters and numbers
-            price = Double.parseDouble(JOptionPane.showInputDialog("Enter Dish's Price"));
-            //validation that a price has been entered
-
-            car = new Car(category, make, model, resNumber, price);
-            cars.add(car);
-
-            JOptionPane.showMessageDialog(null, "Car '" + make + model + "' added to the system");
-
 
         }
+        class ReservationMenu implements ActionListener {
 
-         public void displayCar() {
-            JComboBox carCombo = new JComboBox();
-            JTextArea output = new JTextArea();
 
-            output.setText("Car Details:\n\n");
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame = new JFrame("Reservation Menu");
+                setBounds(400, 100, 500, 500);
+                frame.setSize(600, 400);
+                frame.setLocationRelativeTo(null);
+                setLayout(new FlowLayout());
+                JPanel panel = new JPanel();
+                frame.add(panel);
 
-            if (cars.size() < 1) {
-                JOptionPane.showMessageDialog(null, "No Cars have been added to the system yet.", "Warning", JOptionPane.WARNING_MESSAGE);
-            } else {
-                Iterator<Car> iterator = cars.iterator();
+                makeResButton = new JButton("Make Reservation ");
+                makeResButton.setBounds(100, 80, 80, 25);
+                makeResButton.setBackground(Color.cyan);
+                panel.add(makeResButton);
+                makeResButton.addActionListener(this);
 
-                while (iterator.hasNext()) {
-                    carCombo.addItem(iterator.next().getCarMake() + (iterator.next().getCarModel()));
-                }
-                JOptionPane.showMessageDialog(null, carCombo, "Select Car to view details", JOptionPane.PLAIN_MESSAGE);
-                int selected = carCombo.getSelectedIndex();
-                output.append(cars.get(selected).toString());
+                cancelResButton= new JButton("Cancel Reservation ");
+                cancelResButton.setBounds(100, 80, 80, 25);
+                cancelResButton.setBackground(Color.cyan);
+                panel.add(cancelResButton);
+                cancelResButton.addActionListener(this);
 
-                JOptionPane.showMessageDialog(null, output, "Car Details", JOptionPane.PLAIN_MESSAGE);
+                frame.setVisible(true);
             }
         }
 
-       public void deleteCar() {
+    }
 
-            JComboBox carList = new JComboBox();
 
-            for (Car c : cars) {
-                carList.addItem((c.getCarMake() + c.getCarModel()));
-            }
-            JOptionPane.showMessageDialog(null, "Select car to be removed", "Remove Car", JOptionPane.INFORMATION_MESSAGE);
 
-            JOptionPane.showMessageDialog(null, carList, "Remove Car", JOptionPane.INFORMATION_MESSAGE);
 
-            int selected = carList.getSelectedIndex();
 
-            cars.remove(selected);
+    public static void main(String[] args) {
+        new CarRentalApp();
+    }
+    public void addCar() {
 
-            JOptionPane.showMessageDialog(null, "Car Removed", "Removed", JOptionPane.INFORMATION_MESSAGE);
+        final String[] carsCategory = {"Sports", "Economy", "Premium"};
+        String category;
+        String make;
+        String model;
+        String resNumber;
+        double price;
+
+        category = (String) JOptionPane.showInputDialog(null, "Add Car", "Car", JOptionPane.QUESTION_MESSAGE, null, carsCategory, carsCategory[0]);
+
+        make = JOptionPane.showInputDialog("Enter car Make");
+        if(!make.matches("[a-zA-Z_]+")){
+            JOptionPane.showMessageDialog(null,"Invalid Make entered. Must be only letters ","ERROR",JOptionPane.ERROR_MESSAGE);
         }
+        model = JOptionPane.showInputDialog("Enter car Model");
+        if(!model.matches("[a-zA-Z_]+")){
+            JOptionPane.showMessageDialog(null,"Invalid Model entered. Must be only letters ","ERROR",JOptionPane.ERROR_MESSAGE);
+        }
+        resNumber = JOptionPane.showInputDialog("Enter car Registration Number");
+        if(!resNumber.matches("[0-9]")){
+            JOptionPane.showMessageDialog(null,"Invalid Registration Number Added","ERROR",JOptionPane.ERROR_MESSAGE);
+        }
+        price = Double.parseDouble(JOptionPane.showInputDialog("Enter Car Price Per Day"));
+        //validation that a price has been entered
 
+        car = new Car(category, make, model, resNumber, price);
+        cars.add(car);
 
+        JOptionPane.showMessageDialog(null, "Car '" + make +" "+ model + "' added to the system");
 
 
 
     }
 
+    public void displayCar() {
+        JComboBox carCombo = new JComboBox();
+        JTextArea output = new JTextArea();
+
+        output.setText("Car Details:\n\n");
+
+        if (cars.size() < 1) {
+            JOptionPane.showMessageDialog(null, "No Cars have been added to the system yet.", "Warning", JOptionPane.WARNING_MESSAGE);
+        } else {
+            Iterator<Car> iterator = cars.iterator();
+
+            while (iterator.hasNext()) {
+                carCombo.addItem(iterator.next().getCarMake() + (iterator.next().getCarModel()));
+            }
+            JOptionPane.showMessageDialog(null, carCombo, "Select Car to view details", JOptionPane.PLAIN_MESSAGE);
+            int selected = carCombo.getSelectedIndex();
+            output.append(cars.get(selected).toString());
+
+            JOptionPane.showMessageDialog(null, output, "Car Details", JOptionPane.PLAIN_MESSAGE);
+        }
+
+    }
+
+    public void deleteCar() {
+
+        JComboBox carList = new JComboBox();
+
+        for (Car c : cars) {
+            carList.addItem((c.getCarMake() + c.getCarModel()));
+        }
+        JOptionPane.showMessageDialog(null, "Select car to be removed", "Remove Car", JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, carList, "Remove Car", JOptionPane.INFORMATION_MESSAGE);
+
+        int selected = carList.getSelectedIndex();
+
+        cars.remove(selected);
+
+        JOptionPane.showMessageDialog(null, "Car Removed", "Removed", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+
+
+
+
+
 }
 
-
+//closing } removed here by JB
